b100568edaf16502090dac3222e68f613df9b665
Sarvesh Khetan <53877600+khetansarvesh@users.noreply.github.com>
2024-06-10 18:08:18 +0530

    Update vectorized_sgd.py

--- modelling/regression/multiple_linear_regression/vectorized_sgd.py
+++ modelling/regression/multiple_linear_regression/vectorized_sgd.py
@@ -72,3 +72,74 @@ class StochasticGradientDescent:
     return np.dot(x,self.new_weights.transpose())
 
 # Similaryly you can have SGD implementation with stop criterai based on :- utill the difference between two errors is <= 0.001 i.e negligible
+class StochasticGradientDescent_ErrorDiff_Stop:
+  # constructor
+  def __init__(self):
+    self.X = 0
+
+  # returns nothing
+  def fit(self, X, Y, learning_rate=0.000001, no_of_iterations=10000):
+    self.X = X
+    self.Y = Y
+    # adding bias column to X
+    df = pd.DataFrame(self.X)
+    bias = [1 for i in range(df.shape[0])]
+    df.insert(loc=0, column="bias", value=bias)
+    self.X = np.array(df)
+
+    # taking all [c, m1, m2...] as [0, 0, 0.....] as initial random value
+    # and representing this in a matrix named weights
+    self.current_weights = np.zeros((self.X.shape[1]))
+
+    # calculating initial error using the formula
+    # (error) = (X.(theta)t - (Y)t).(X.(theta)t - (Y)t)t
+    # -- t means transpose
+    # - some people divide this by 2
+    self.current_error = (np.dot(np.dot(self.X, self.current_weights.transpose()) - self.Y.transpose(),
+                               (np.dot(self.X, self.current_weights.transpose()) - self.Y.transpose()).transpose())) / 2
+    self.error_diff = self.current_error  # to keep a count of iterations that has occured
+    self.no_of_iteration = 0
+
+    # making a list to store error every 50 iterations
+    self.error_every_50_iteration_list = list()
+    self.error_every_50_iteration_list.append(self.current_error)
+
+    # iterating until the difference between two errors is <= 0.00000001
+    # i.e negligible
+    while self.error_diff > 0.00000001:
+      # generating a random point
+      self.Xp = list()
+      self.Yp = list()
+      random_no = random.randint(0, len(self.Y) - 1)
+      self.Xp.append(self.X[random_no])
+      self.Yp.append(self.Y[random_no])
+
+      # converting list into numpy array cause numpy is very fast
+      self.Xp = np.array(self.Xp)
+      self.Yp = np.array(self.Yp)
+
+      # calculating (theta)new = (theta)current - (leaning rate)(derivative term)
+      # (derivative term) = 2*(Xp)t.(Xp.(current_weights)-(Yp)t)
+      # --usually people divide derivative term with (2)*(no of datapoints)
+      # --some people divide it by 2
+      self.new_weights = self.current_weights - (learning_rate) * (
+          np.dot(self.Xp.transpose(), (np.dot(self.Xp, self.current_weights) - self.Yp.transpose())))
+
+      # calculating error using formula
+      # (error due to all points) = (X.(theta)t - (Y)t).(X.(theta)t - (Y)t)t
+      # -- t means transpose
+      # -- some people divide it by 2
+      self.new_error = (np.dot(np.dot(self.X, self.new_weights.transpose()) - self.Y.transpose(),
+                             (np.dot(self.X, self.new_weights.transpose()) - self.Y.transpose()).transpose())) / 2
+
+      # updating the error difference
+      self.error_diff = self.current_error - self.new_error
+      self.current_error = self.new_error
+      self.current_weights = self.new_weights
+
+      # counting the no of iteration taken to run the algorithm
+      self.no_of_iteration = self.no_of_iteration + 1
+
+      # storing the error after every 50 iterations
+      if self.no_of_iteration % 50 == 0:
+        self.error_every_50_iteration_list.append(self.new
